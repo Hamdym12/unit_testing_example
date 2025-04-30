975660c71bfa56dc97dab2963cc2cf47bc4ae01e
Hamdy Mohamed <hamdym12552@yahoo.com>
2025-04-30 08:10:35 +0300

    add counter_test

---
+++ test/counter_test/counter_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_testing_example/counter_example.dart';
+
+void main(){
+  group('Counter',(){
+    late Counter counter;
+
+    setUp((){
+      counter = Counter();
+    });
+
+    test('initial count should be zero',(){
+      expect(counter.count,0);
+    });
+
+    test('increment should increment count by one',(){
+      counter.increment();
+      expect(counter.count,1);
+    });
+
+    test('decrement should decrement count by one',(){
+      counter.decrement();
+      expect(counter.count,-1);
+    });
+
+  });
+}
